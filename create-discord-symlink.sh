09a34acd884925c0c6c8664dcf3fd31f3ca93080
deezhizyu <kalenskuja@gmail.com>
2025-05-30 22:10:39 +0200

    Smoother animations

--- create-discord-symlink.sh
+++ create-discord-symlink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ln -sf $XDG_RUNTIME_DIR/.flatpak/dev.vencord.Vesktop/xdg-run/discord-ipc-0 $XDG_RUNTIME_DIR
+ln -sf $XDG_RUNTIME_DIR/.flatpak/io.github.milkshiift.GoofCord/xdg-run/discord-ipc-0 $XDG_RUNTIME_DIR
