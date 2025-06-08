faa289cba3d3ee8dd13d395f9f9275d9384a531d
deezhizyu <kalenskuja@gmail.com>
2025-06-08 13:30:04 +0200

    Implement video wallpaper switching. Base from code by @imbypass

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,3 +1,4 @@
 alias zed="zeditor"
 alias kan="~/.local/bin/generate-kan"
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
--- .config/walker/scripts/wallpaper.sh
+++ .config/walker/scripts/wallpaper.sh
@@ -1,12 +1,25 @@
-list=$(find -L "$HOME/.local/share/wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \))
+list=$(find -L "$HOME/.local/share/wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.mp4" \))
 
 while walls= read -r path; do
-  name=$(b=${path##*/}; echo ${b%.*})
+    name=$(b=${path##*/}; echo ${b%.*})
 
-  # now thats some grade-A fuckery right there! learn bash betr bro!
-  name=${name//_/ };
-  name=${name//-/ };
-  name=${name//   / };
+    # now thats some grade-A fuckery right there! learn bash betr bro!
+    name=${name//_/ };
+    name=${name//-/ };
+    name=${name//   / };
 
-  printf "image=$path;label=${name};exec=swww img --transition-fps 144 --transition-duration 1 -t fade $path && cp $path $HOME/.local/share/wallpapers/.wallpaper && $HOME/.local/bin/bde u colors && $HOME/.local/bin/bde rl pcmanfm;\n"
+    # Check if the file is an mp4 video
+    if [[ "${path,,}" == *_preview.jpg ]]; then
+        continue
+    elif [[ "${path,,}" == *.mp4 ]]; then
+        preview_path="${path}_preview.jpg"
+
+        if ! [[ -f $preview_path ]]; then
+            ffmpeg -i $path -vframes 1 -q:v 2 $preview_path > /dev/null 2>&1
+        fi
+
+        printf "image=$preview_path;label=${name};exec=$HOME/.local/bin/harmonyctl vwp $path;\n"
+    else
+        printf "image=$path;label=${name};exec=$HOME/.local/bin/harmonyctl wp $path;\n"
+    fi
 done <<< "$list"
